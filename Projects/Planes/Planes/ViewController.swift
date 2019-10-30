05d899fe1bacdeb44ef77c8a498f63d82742962c
Ludovic Ollagnier <ludovic@ollagnier.me>
2019-10-30 15:33:55 +0100

    Cleanup

--- Projects/Planes/Planes/ViewController.swift
+++ Projects/Planes/Planes/ViewController.swift
@@ -10,30 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var nameLabel: UILabel!
-
-    var name: String = "Ludovic"
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
-        view.backgroundColor = UIColor.red
-
     }
-
-    @IBAction func sayHello(_ sender: UIButton) {
-
-        nameLabel.text = nameTextField.text
-        sender.isEnabled = false
-
-        let double = 35.987
-        let strValue = String(double)
-        let strValue2 = "\(double)"
-
-        let doubleValue = Double("45.98")
-    }
-
 }
 
